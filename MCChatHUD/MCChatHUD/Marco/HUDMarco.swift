e1a5420fb638ec5dd18cbcce6bb52736cf857ce1
杜玮 <Duwei_1997@163.com>
2018-01-30 15:26:29 +0800

    Add: 添加全局定义

---
+++ MCChatHUD/MCChatHUD/Marco/HUDMarco.swift
@@ -0,0 +1,19 @@
+//
+//  HUDMarco.swift
+//  MCChatHUD
+//
+//  Created by duwei on 2018/1/30.
+//  Copyright © 2018年 Dywane. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+//MARK: - UI Marco
+
+/// 界面宽度
+internal let HUDWidth: CGFloat = 170
+/// 界面高度
+internal let HUDHeight: CGFloat = 78
+/// 界面圆角
+internal let HUDCornerRadius: CGFloat = 38
