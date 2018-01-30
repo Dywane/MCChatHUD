3a2c764c30c0cadf0169f97111287bb0f2f67f73
杜玮 <Duwei_1997@163.com>
2018-01-30 15:09:29 +0800

    Init: project

---
+++ MCChatHUD/MCChatHUD/View/MCProgressView.swift
@@ -0,0 +1,21 @@
+//
+//  MCProgressView.swift
+//  MCChatHUD
+//
+//  Created by duwei on 2018/1/30.
+//  Copyright © 2018年 Dywane. All rights reserved.
+//
+
+import UIKit
+
+class MCProgressView: UIImageView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ MCChatHUD/MCChatHUD/View/MCVolumeView.swift
@@ -0,0 +1,21 @@
+//
+//  MCVolumeView.swift
+//  MCChatHUD
+//
+//  Created by duwei on 2018/1/30.
+//  Copyright © 2018年 Dywane. All rights reserved.
+//
+
+import UIKit
+
+class MCVolumeView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
