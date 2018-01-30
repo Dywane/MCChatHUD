b4c98f950633b7b02ac31e5c48361e67251be540
杜玮 <Duwei_1997@163.com>
2018-01-30 15:45:49 +0800

    Update: MCRecordHUD

--- MCChatHUD/MCChatHUD/View/MCRecordHUD.swift
+++ MCChatHUD/MCChatHUD/View/MCRecordHUD.swift
@@ -10,12 +10,71 @@ import UIKit
 
 class MCRecordHUD: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    //MARK: - Public Properties
+    /// 提示Label
+    public let titleLabel = UILabel()
+    
+    //MARK: - Private Properties
+    private let progress = MCProgressView(frame: CGRect(x: 0, y: 0, width: HUDWidth, height: HUDHeight))
+    private let volume = MCVolumeView(frame: CGRect(x: 56, y: 0, width: VolumeViewWidth, height: VolumeViewHeight))
+    
+    //MARK: Methods
+    public func startCounting() {
+        progress.countingAnimate()
+        titleLabel.text = "Slide up to cancel"
     }
-    */
+    
+    public func stopCounting() {
+        progress.stopAnimate()
+    }
+    
+    //MARK: - Init
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.frame.size.width = HUDWidth
+        self.frame.size.height = HUDHeight
+        center = CGPoint(x: ScreenWidth/2, y: ScreenHeight/2 - 50)
+        backgroundColor = UIColor.clear
+        addSubview(progress)
+        addSubview(volume)
+        setUpLabel()
+        addSubview(titleLabel)
+        setUpShadow()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
+}
+
+// MARK: - Setup
+extension MCRecordHUD {
+    
+    private func setUpLabel() {
+        titleLabel.textColor = UIColor.white
+        titleLabel.font = UIFont.systemFont(ofSize: 12, weight: .bold)
+        titleLabel.textAlignment = .center
+        titleLabel.backgroundColor = UIColor.clear
+        titleLabel.frame = CGRect(x: 25, y: 46, width: 120, height: 14)
+    }
+    
+    private func setUpShadow() {
+        
+        let progessViewBounds = progress.frame
+        let shadowWidth = progessViewBounds.size.width * 0.85
+        let shadowHeight = progessViewBounds.size.height * 0.75
+        
+        let shadowPath = UIBezierPath(roundedRect: CGRect(x: progessViewBounds.origin.x + (progessViewBounds.width - shadowWidth) * 0.5,
+                                                          y: progessViewBounds.origin.y + 20,
+                                                          width: shadowWidth,
+                                                          height: shadowHeight),
+                                      cornerRadius: progress.layer.cornerRadius)
+        
+        layer.shadowColor = UIColor(red: 0.29, green: 0.29, blue: 0.29, alpha: 1).cgColor
+        layer.shadowPath = shadowPath.cgPath
+        layer.shadowOpacity = 0.5
+        layer.shadowRadius = 8
+        layer.shadowOffset = CGSize(width: 0, height: 10)
+    }
 }
